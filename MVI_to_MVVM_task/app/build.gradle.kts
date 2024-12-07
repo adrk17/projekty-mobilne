5681551003991ad670fc13fccb7e102b7c9bf6f5
kkasj <k.kostowskiof@gmail.com>
2024-12-07 19:15:44 +0100

    34 to 35

--- MVI_to_MVVM_task/app/build.gradle.kts
+++ MVI_to_MVVM_task/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mvi_to_mvvm_task"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mvi_to_mvvm_task"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
