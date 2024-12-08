2045b4ec414a76c9b7e0147b0cb6a7f67b4d541a
adrk17 <adrian.krupa17@gmail.com>
2024-12-08 14:50:29 +0100

    Refactor task

--- SlidesGallery/app/src/main/java/com/example/slidesgallery/view/MainActivity.kt
+++ SlidesGallery/app/src/main/java/com/example/slidesgallery/view/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.slidesgallery.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.slidesgallery.databinding.ActivityMainBinding
 import com.example.slidesgallery.model.Slide
@@ -15,11 +16,26 @@ class MainActivity : AppCompatActivity() {
     // 1. Usuń logikę zarządzania slajdami z MainActivity.
     // 2. Obserwuj LiveData z ViewModel, aby aktualizować widoki (np. obraz w ImageView).
     // 3. Przenieś wszelkie zmienne i metody związane z logiką obsługi slajdów do ViewModel.
+    /*
+        com.example.slidesgallery:
+        - model
+            - Slide.kt
+            - SlidesDataProvider.kt
+        - view
+            - MainActivity.kt TODO: Przenieś logikę obsługi slajdów do ViewModel.
+        - viewmodel
+            - SlidesViewModel.kt TODO: Utwórz ViewModel, który będzie zarządzał listą slajdów.
+     */
 
     private lateinit var layoutBinding: ActivityMainBinding
+    // Utworzenie instancji ViewModel za pomocą delegacji przez viewModels(). W takim przypadku ViewModel jest tworzona i zarządzana przez bibliotekę, zachowując swój stan w większej niezależności od cyklów życia Activity.
     //private val viewModel : SlidesViewModel by viewModels()
 
-    private val currentSlide = MutableLiveData<Slide>()
+    // Zmienna prywatna, typu MutableLiveData, przechowująca aktualny slajd, zmieniana wewnątrz ViewModel.
+    private val _currentSlide = MutableLiveData<Slide>()
+    // Zmienna publiczna, typu LiveData, niezmienna, przechowująca aktualny slajd do obserwacji w innych częściach aplikacji.
+    val currentSlide: LiveData<Slide> = _currentSlide
+
     private val slides = SlidesDataProvider().getSlides()
     private var currentIndex = 0
 
@@ -36,21 +52,18 @@ class MainActivity : AppCompatActivity() {
             moveSlide(Direction.RIGHT)
         }
 
+        // Część logiki biznesowej związana z obsługą slajdów.
         updateSlide(0)
     }
 
     override fun onResume() {
         super.onResume()
 
-        getCurrentSlide().observe(this, { slide ->
+        currentSlide.observe(this, { slide ->
             layoutBinding.imageView.setImageDrawable(
                 ResourcesCompat.getDrawable(resources, slide.photo, applicationContext.theme)
             )
         })
-    }
-
-    fun getCurrentSlide(): MutableLiveData<Slide> {
-        return currentSlide;
     }
 
     fun moveSlide(direction: Direction){
@@ -62,7 +75,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateSlide(index : Int){
         currentIndex = (index + slides.size) % slides.size
-        currentSlide.value = slides[currentIndex]
+        _currentSlide.value = slides[currentIndex]
     }
 
     enum class Direction(){
